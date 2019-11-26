6a82d4abaeb2e2b269856ef48edf3a147e034ce0
Achilis <tanguachilis@gmail.com>
2019-11-26 02:04:31 +0100

    formated the registration screen

--- lib/screens/signUp/signUp_screen.dart
+++ lib/screens/signUp/signUp_screen.dart
@@ -3,7 +3,7 @@ import 'package:relevant_systems/components/normalInputField.dart';
 import 'package:relevant_systems/components/passwordInputField.dart';
 
 class SignUPScreen extends StatelessWidget {
-  static String id='SignUPScreen';
+  static String id = 'SignUPScreen';
   @override
   Widget build(BuildContext context) {
     var screensize = MediaQuery.of(context).size;
@@ -11,9 +11,7 @@ class SignUPScreen extends StatelessWidget {
       alignment: Alignment.bottomCenter,
       //margin: EdgeInsets.only(bottom: screensize.height / 40),
 
-      padding: EdgeInsets.symmetric(
-
-          horizontal: screensize.width / 30),
+      padding: EdgeInsets.symmetric(horizontal: screensize.width / 30),
       width: screensize.width / 1.13,
       constraints: BoxConstraints(
         minHeight: screensize.height / 1.6,
@@ -25,45 +23,60 @@ class SignUPScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          SizedBox(height: screensize.height / 30,),
-
-          Text('Relevant Systems',  style: TextStyle(fontSize: 30, color: Colors.grey.shade800, fontFamily: 'Gotham-Medium', fontWeight: FontWeight.w900),),
-          SizedBox(height: screensize.height/20,),
-
-          NormalInputField(labelText: 'User Name',),
+          SizedBox(
+            height: screensize.height / 30,
+          ),
+          Text(
+            'Relevant Systems',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.grey.shade800,
+                fontFamily: 'Gotham-Medium',
+                fontWeight: FontWeight.w900),
+          ),
+          SizedBox(
+            height: screensize.height / 20,
+          ),
+          NormalInputField(
+            labelText: 'User Name',
+          ),
           Padding(padding: EdgeInsets.all(10)),
-
-          NormalInputField(labelText: 'Email Address',),
+          NormalInputField(
+            labelText: 'Email Address',
+          ),
           Padding(padding: EdgeInsets.all(10)),
-
-          PasswordInputField(labelText: 'Password',),
-
-          SizedBox(height: screensize.height/40),
-
+          PasswordInputField(
+            labelText: 'Password',
+          ),
+          SizedBox(height: screensize.height / 40),
           MaterialButton(
             minWidth: screensize.width / 1.13 - screensize.width / 15,
-            padding: EdgeInsets.symmetric(vertical:screensize.width / 20 ),
-            child: Text('SIGN UP', style: TextStyle(fontSize: 20,color: Theme.of(context).primaryColor, fontFamily: 'Gotham-Medium'),),
+            padding: EdgeInsets.symmetric(vertical: screensize.width / 20),
+            child: Text(
+              'SIGN UP',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                  fontFamily: 'Gotham-Medium'),
+            ),
             color: Theme.of(context).primaryColorDark,
-            onPressed: (){
-
-            },
+            onPressed: () {},
           ),
-
-          SizedBox(height: screensize.height/40),
-
+          SizedBox(height: screensize.height / 40),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Already have an account?', style:TextStyle(color: Colors.grey.shade400,fontSize: 16)),
+              Text('Already have an account?',
+                  style: TextStyle(color: Colors.grey.shade400, fontSize: 16)),
               FlatButton(
-                child: Text('Sign in', style: TextStyle(fontSize: 16, color: Theme.of(context).primaryColor,fontWeight: FontWeight.bold)),
+                child: Text('Sign in',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold)),
               )
             ],
           ),
-
-
-
         ],
       ),
     );
