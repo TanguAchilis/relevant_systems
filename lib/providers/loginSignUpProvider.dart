c3a597545eaddcb9d3514805995e6c2651a44f7c
Michel Ndjock <ndjockjunior@gmail.com>
2019-11-27 12:57:26 +0100

    updates

--- lib/providers/loginSignUpProvider.dart
+++ lib/providers/loginSignUpProvider.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:relevant_systems/screens/login/login_screen.dart';
 
-class LoginSignUpProvider with ChangeNotifier{
-  Widget _signUpOrLogin=LoginScreen();
+class LoginSignUpProvider with ChangeNotifier {
+  Widget _signUpOrLogin = LoginScreen();
 
-  set toggleLoginSignUP(Widget value){
-    _signUpOrLogin=value;
+  set toggleLoginSignUP(Widget value) {
+    _signUpOrLogin = value;
     notifyListeners();
   }
 
@@ -19,5 +19,4 @@ class LoginSignUpProvider with ChangeNotifier{
   //variables for the login screen
   String loginEmail;
   String loginPassword;
-
 }
